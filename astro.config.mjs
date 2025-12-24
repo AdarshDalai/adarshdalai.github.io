aae5a5c6a6cc8da0fca8d3ce9ed8a2f1636db0d1
AdarshDalai <adarshkumar.dalai@gmail.com>
2025-12-25 00:55:53 +0530

    page config update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://adarshdalai.github.io',
+  site: 'https://adarsh.page',
 });
